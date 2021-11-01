a13aad4d45a2a1dbb7288a1b007c1021a9c16f93
chiyuCoder <chiyu_xinyi@163.com>
2021-11-01 10:35:08 +0800

    nanText Default

--- src/NumTransferTool.ts
+++ src/NumTransferTool.ts
@@ -5,20 +5,20 @@ export class NumTransferTool {
     constructor(public defaultNanText: string | number = "") {
     }
 
-    floatVal(text: string | number, nanText: string | number = this.defaultNanText): string | number {
-        return floatVal(text, nanText);
+    floatVal(text: string | number, nanText?: string | number): string | number {
+        return floatVal(text, nanText ?? this.defaultNanText);
     }
 
-    intVal(text: string | number, nanText: string | number = this.defaultNanText): string | number {
-        return intVal(text, nanText);
+    intVal(text: string | number, nanText?: string | number): string | number {
+        return intVal(text, nanText ?? this.defaultNanText);
     }
 
-    toFixed(text: string | number, saveNum: number = this.defaultSaveNum,  nanText: string | number = this.defaultNanText): string | number {
-        return toFixed(text, saveNum, nanText);
+    toFixed(text: string | number, saveNum: number = this.defaultSaveNum,  nanText?: string | number): string | number {
+        return toFixed(text, saveNum, nanText ?? this.defaultNanText);
     }
 
-    floatNum(text: string | number, saveNum: number = this.defaultSaveNum,  nanText: string | number = this.defaultNanText): string | number {
-        return floatNum(text, saveNum, nanText);
+    floatNum(text: string | number, saveNum: number = this.defaultSaveNum,  nanText?: string | number): string | number {
+        return floatNum(text, saveNum, nanText ?? this.defaultNanText);
     }
 }
 
