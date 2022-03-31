5f5c8696830751c7833c08ce7c6f937a46aa68cf
chiyuCoder <chiyu_xinyi@163.com>
2022-03-31 12:29:30 +0800

    add test

---
+++ test/num.spec.ts
@@ -0,0 +1,18 @@
+/**
+ * @jest-environment jsdom
+ */
+import {intVal, stringifyNumber} from "../src";
+
+describe("num spec", () => {
+    it("num.stringifyNumber", () => {
+        expect(stringifyNumber(1e-9)).toBe("0.000000001");
+    });
+});
+
+describe("int val", () => {
+   it("num.intVal", () => {
+       expect(intVal(1e-9, "")).toBe(0);
+       expect(intVal("1e-9", "")).toBe(1);
+       expect(intVal(1234567890.234e-9, "")).toBe(1);
+   })
+});
