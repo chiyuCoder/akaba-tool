f078f686edc1fe59ccf08f499800f0c35190e83f
chiyuCoder <chiyu_xinyi@163.com>
2022-04-02 10:02:58 +0800

    getRangeOfList

--- src/arrayLike.ts
+++ src/arrayLike.ts
@@ -1,5 +1,12 @@
+import {floatVal} from "./num";
+
 export namespace NSFuncArrayLike {
     export type TLoopFunc<T> = (item: T , index: number, arrLike: ArrayLike<T>) => any;
+
+    export interface IRange {
+        min: number;
+        max: number;
+    }
 }
 
 export function doLoop<T>(arrLike: ArrayLike<T>, loopFunc: NSFuncArrayLike.TLoopFunc<T>) {
@@ -10,4 +17,27 @@ export function doLoop<T>(arrLike: ArrayLike<T>, loopFunc: NSFuncArrayLike.TLoop
             return;
         }
     }
+}
+
+export function getRangeOfList<T>(arrLike: ArrayLike<T>, valueFunc?: (item: T, index: number) => number): NSFuncArrayLike.IRange {
+    let min: number = NaN;
+    let max: number = NaN;
+    doLoop(arrLike, (item, index) => {
+       let valNum: number = NaN;
+       if (valueFunc) {
+           valNum = valueFunc(item, index);
+       } else {
+           valNum = floatVal(item as any as string, NaN) as number;
+       }
+       if (isNaN(min) || min > valNum) {
+           min = valNum;
+       }
+       if (isNaN(max) || max < valNum) {
+           max = valNum;
+       }
+    });
+    return {
+        min,
+        max
+    };
 }
