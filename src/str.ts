68d15db23907d7398c94e985d6a2712e4170fdad
chiyuCoder <stevindjimmy@gmail.com>
2023-05-22 11:01:02 +0800

    add toShowString&toNotEmptyString

--- src/str.ts
+++ src/str.ts
@@ -7,3 +7,24 @@ export {
   stringify,
   rejoinString,
 } from "./str-relate/get-words";
+
+/**
+ * @since 1.3.3
+ * @param str
+ * @param whenNull
+ */
+export function toShowString(str: any, whenNull: string = ""): string {
+  if (str == null) {
+    return whenNull;
+  }
+  return str.toString();
+}
+
+/**
+ * @since 1.3.3
+ * @param str
+ * @param whenEmpty
+ */
+export function toNotEmptyString(str: any, whenEmpty: string = "-"): string {
+  return toShowString(str) || whenEmpty;
+}
