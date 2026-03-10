72a893c765cc794d334ef57446e7f3079a1e47a8
chiyuCoder <stevindjimmy@gmail.com>
2026-03-10 14:45:12 +0800

    callFunc

--- src/func-helper.ts
+++ src/func-helper.ts
@@ -1,9 +1,14 @@
 /**
- * @since 1.4.15
+ * @since 1.4.16
  * @param func 
  * @param parameters 
  * @returns 
  */
-export function callFunc<R, P extends Array<unknown>>(func: (...parameters: P) => R, parameters: P): R {
-  return func(...parameters);
+export function callFunc<R>(func: () => R): R 
+export function callFunc<R, P extends Array<unknown>>(func: (...parameters: P) => R, parameters: P): R
+export function callFunc<R, P extends Array<unknown>>(func: ((...parameters: P) => R) | (() => R), parameters?: P): R {
+  if (Array.isArray(parameters)) {
+    return func(...parameters);
+  }
+  return func();
 }
